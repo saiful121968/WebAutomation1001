f4737c128c86c15f54996a7305d7f5fa3fe94a69
saiful <saiful121968@gmail.com>
2018-02-15 18:53:10 -0500

    new file added

--- WebAutomation1001/src/test/java/programming_1/Facebook2.java
+++ WebAutomation1001/src/test/java/programming_1/Facebook2.java
@@ -11,8 +11,11 @@ public class Facebook2 {
 		System.setProperty("webdriver.chrome.driver", "C:/Users/Saiful/Utilities/chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("http://www.facebook.com/");
-		driver.findElement(By.id("email")).sendKeys("saifil@gmail.com");
+		driver.findElement(By.id("email")).sendKeys("bdindira");
+		driver.findElement(By.name("pass")).sendKeys("bd1968");
+		driver.findElement(By.xpath("//*[@id=\'u_0_2\']")).click();
 		Thread.sleep(2000);
+		//driver.close();
 
 	}
 
